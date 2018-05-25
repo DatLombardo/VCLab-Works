0b2f262feb5b25ee804ea678d439951f1d80ccf4
DatLombardo <Michael.Lombardo@uoit.net>
2018-05-25 13:21:39 -0400

    Feature extraction with writing to csv

--- Feature-Extraction/featureExtraction.py
+++ Feature-Extraction/featureExtraction.py
@@ -2,26 +2,25 @@ import cv2
 from darkflow.net.build import TFNet
 import numpy as np
 import time
+import csv
 
 option = {
     'model': 'cfg/yolo.cfg',
     'load': 'bin/yolov2.weights',
-    'threshold': 0.15,
+    'threshold': 0.65,
     'gpu': 1.0,
 }
 
 def frameLabel(frame):
-    label = []
-    values = []
     #Convert frame to gray for blurry and uniform labels
     grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     #label = darkLabel(frame, (blurryLabel(grayFrame, (uniformLabel(grayFrame, label, values)))))
-    label, values = darkLabel(frame, label, values)
-    label, values = blurryLabel(grayFrame, label, values)
-    label, values = uniformLabel(grayFrame, label, values)
-    return label, values
+    Y = darkLabel(frame)
+    S = blurryLabel(grayFrame)
+    U = uniformLabel(grayFrame)
+    return [Y, S, U]
 
-def darkLabel(frame, label, values):
+def darkLabel(frame):
     #Normalize to 0.0 - 1.0
     image = frame / 255.0
 
@@ -30,16 +29,13 @@ def darkLabel(frame, label, values):
                 (image[:,:,1].mean() * 0.7152) +
                 (image[:,:,2].mean() * 0.2126))
 
-    values.append(round(Y, 4))
     #Compare Y value to empirically determined Sigma
     if (Y <= 0.097):
-        label.append("dark")
         print("Frame is classified as dark.")
-    #print("Computed Y Value: " + str(Y))
 
-    return label, values
+    return round(Y, 5)
 
-def blurryLabel(frame, label, values):
+def blurryLabel(frame):
     #Horizontal Image Gradient
     gaussianX = cv2.Sobel(frame, cv2.CV_32F, 1, 0)
     #Vertical Image Gradient
@@ -48,19 +44,16 @@ def blurryLabel(frame, label, values):
     #Compute S Coefficient - mean(Gx^2 + Gy^2)
     S = np.mean((gaussianX * gaussianX) + (gaussianY * gaussianY))
 
-    values.append(round(S, 4))
     #Compare S value to empirically determined Beta
     if (S <= 502.32):
-        label.append("blurry")
         print("Frame is classified as blurry.")
-    #print("Computed S Value: " + str(S))
 
-    return label, values
+    return round(S, 5)
 
-def uniformLabel(frame, label, values):
+def uniformLabel(frame):
     #128-bin histogram -> flatten into 1D array -> normalize
     #To get the max range of values use: np.ptp(gray_image)
-    hist = (cv2.calcHist([frame],[0],None,[127],[0.0,255.0]).flatten())/ 255.0
+    hist = (cv2.calcHist([frame],[0],None,[127],[0.0,255.0]).flatten())/ 255.087
 
     #Sort in descending order
     hist[::-1].sort()
@@ -71,20 +64,17 @@ def uniformLabel(frame, label, values):
     #Compute U coefficient - sum of top 5th percentile
     U = 1 - np.sum(ratios[0:int(np.floor(0.05 * 128))])
 
-    values.append(round(U, 4))
     #Compare U value to empirically determined Y
     if (U < 0.2):
-        label.append("uniform")
         print("Frame is classified as uniform.")
-    #print("Computed U Value: " + str(U))
-    return label, values
 
+    return round(U, 5)
 
 tfnet = TFNet(option)
 
 #Load mp4 file
-capture = cv2.VideoCapture('../Testing Data/leaves_jumping.mp4')
-capture = cv2.VideoCapture("../Testing Data/playing_ball.mp4")
+capture = cv2.VideoCapture('../Testing-Data/leaves_jumping.mp4')
+#capture = cv2.VideoCapture("../Testing-Data/playing_ball.mp4")
 colours = [tuple(255 * np.random.rand(3)) for i in range(5)]
 
 #Definition of empty container for frame / label storage
@@ -96,32 +86,31 @@ y = 50
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 while (capture.isOpened()):
-    stime = time.time()
     ret, currFrame = capture.read()
     if ret:
+        frameObjects = []
+        #Yolo Object Detection
+
         results = tfnet.return_predict(currFrame)
         for colour, result in zip(colours, results):
+            #Extract X,Y Position of Top-Left Bounding Box, Bottom-Right Bounding Box
             tl = (result['topleft']['x'], result['topleft']['y'])
             br = (result['bottomright']['x'], result['bottomright']['y'])
-            label = result['label']
             currFrame = cv2.rectangle(currFrame, tl, br, colour, 3)
-            currFrame = cv2.putText(currFrame, label, tl, font, 0.7, (0, 0, 0), 2)
-        label, values = frameLabel(np.float32(currFrame))
-        np.append(frameCollection, [currFrame, label, values])
+            currFrame = cv2.putText(currFrame, result['label'], tl, font, 0.7, (0, 0, 0), 2)
+            #Generate Object list, [label, confidence, top-left bb, bottom-right bb]
+            frameObjects.append([result['label'], result['confidence'], tl, br])
+
+        values = frameLabel(np.float32(currFrame))
+
+        #Dark (Y) [0], Blurry (S) [1], Uniform (U) [2], #Detected Objects, Object list
+        frameCollection.append([currFrame, values[0], values[1], values[2], len(frameObjects), frameObjects])
+
         #Add frame labelling values to display,
-        #Teal indicates a valid value, and Red indicates a value within threshold.
-        if ('dark' in label):
-            cv2.putText(currFrame,"Dark(Y) =" + str(values[0]),(x,y),font,0.5,(0,0,255),1)
-        else:
-            cv2.putText(currFrame,"Dark(Y) =" + str(values[0]),(x,y),font,0.5,(255,246,0),1)
-        if ('blurry' in label):
-            cv2.putText(currFrame,"Blurry(S) =" + str(values[1]),(x,y+20),font,0.5,(0,0,255),1)
-        else:
-            cv2.putText(currFrame,"Blurry(S) =" + str(values[1]),(x,y+20),font,0.5,(255,246,0),1)
-        if ('uniform' in label):
-            cv2.putText(currFrame,"Uniform(U) =" + str(values[2]),(x,y+40),font,0.5,(0,0,255),1)
-        else:
-            cv2.putText(currFrame,"Uniform(U) =" + str(values[2]),(x,y+40),font,0.5,(255,246,0),1)
+        cv2.putText(currFrame,"Dark(Y) =" + str(values[0]),(x,y),font,0.5,(255,246,0),1)
+        cv2.putText(currFrame,"Blurry(S) =" + str(values[1]),(x,y+20),font,0.5,(255,246,0),1)
+        cv2.putText(currFrame,"Uniform(U) =" + str(values[2]),(x,y+40),font,0.5,(255,246,0),1)
+
         #Display current frame
         cv2.imshow('frame', currFrame)
         if cv2.waitKey(25) & 0xFF == ord('q'):
@@ -130,3 +119,14 @@ while (capture.isOpened()):
         capture.release()
         cv2.destroyAllWindows()
         break
+
+#Open feature vector container, and empty.
+featureFile = open('featureVector.csv', 'w')
+featureFile.truncate()
+
+#Write to csv
+with featureFile:
+    writer = csv.writer(featureFile)
+    writer.writerows(frameCollection)
+
+featureFile.close()
